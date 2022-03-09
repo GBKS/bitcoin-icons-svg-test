8e50bb21ce8b62cd8970550bebb528003e8c86fc
GBKS <chri@sto.ph>
2022-03-09 14:52:26 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # bitcoin-icons-svg-test
-Test project for the SVG module of bitcoinicons.com.
+
+A super simple test project for the SVG module of bitcoinicons.com.
+
+To be used in conjunction with the [release process](https://github.com/BitcoinDesign/Bitcoin-Icons/blob/main/RELEASES.md) to quickly launch a site that shows all the icons. This ensures things work on the tech side, and allows for visual inspection.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="content">
+    <div class="icons -outline">
+      <div
+        v-for="(item, index) in outlineIcons"
+        v-html="item.svg"
+        :key=item.name
+      />
+    </div>
+    <div class="icons -filled">
+      <div
+        v-for="(item, index) in filledIcons"
+        v-html="item.svg"
+        :key=item.name
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import * as outlineIcons from "@bitcoin-design/bitcoin-icons-svg/outline/index.js";
+import * as filledIcons from "@bitcoin-design/bitcoin-icons-svg/filled/index.js";
+
+export default {
+  name: 'App',
+  components: {
+  },
+
+  data() {
+    return {
+      outlineIcons,
+      filledIcons
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  flex-basis: 800px;
+  flex-grow: 0;
+  max-width: 800px;
+  gap: 40px;
+}
+
+.icons {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.icons div {
+  padding: 5px;
+}
+
+.icons div svg {
+  width: 30px;
+  height: 30px;
+}
+</style>
